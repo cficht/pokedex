c3b911c42fadc90abf086247ea4f2c1b6c07bccc
cficht <chris.ficht@gmail.com>
2020-02-12 17:45:06 -0800

    incorporated api

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import './App.css';
 import Header from './Header.js';
 import PokeList from './PokeList.js';
+import request from 'superagent';
 
 export default class App extends React.Component {
+    state = { pokeDex: [] }
+
+    async componentDidMount() {
+		const pokeOnline = await request.get('https://alchemy-pokedex.herokuapp.com/api/pokedex?perPage=150');
+    this.setState({ pokeDex: pokeOnline.body.results})
+    console.log(this.state.pokeDex);
+}
     render() {
         return(
             <div id="app-div">
               APP
               <Header />
-              <PokeList />
+              <PokeList pokedex={this.state.pokeDex ? this.state.pokeDex : 'loading' }/>
             </div>
         );
     }
--- src/PokeList.js
+++ src/PokeList.js
@@ -4,12 +4,8 @@ import PokeItem from './PokeItem.js'
 import data from './data.js';
 
 export default class PokeList extends Component {
-
-    
-
     render() {
-        const pokeNode = data.map(pokemon => <PokeItem pokemon={pokemon}></PokeItem>)
-        console.log(pokeNode);
+        const pokeNode = this.props.pokedex.map(pokemon => <PokeItem pokemon={pokemon}></PokeItem>)
         return(
             <div id="list-div">
                 <div id="list-top">
