0085c4c87e4b56226a69c04b1d774bca81413cf8
cficht <chris.ficht@gmail.com>
2020-02-13 10:17:12 -0800

    updated the pokeitem test

--- src/PokeItem.test.js
+++ src/PokeItem.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PokeItem from './PokeItem';
 import renderer from "react-test-renderer";
+import data from './data.js';
 
 test('renders PokeItem.js correctly', () => {
   const tree = renderer
-  .create(<PokeItem pokemon={[]} />)
+  .create(<PokeItem pokemon={data} />)
   .toJSON();
   expect(tree).toMatchSnapshot();
 });
